aba3dae55384e474c63cfb483ada561e44451505
Manas Aggarwal <88250052+Manasagg17@users.noreply.github.com>
2021-08-18 22:49:05 +0530

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+// connect to database
+mongoose.connect("mongodb://127.0.0.1:27017/Task");
+
+//acquire the connection(to check if it is successful)
+const db = mongoose.connection;
+
+//error
+db.on("error", console.error.bind(console, "error connecting to db"));
+
+//up and runnung then print this mssg
+db.once("open", function () {
+  console.log("Successfully connected to the database :: MongoDb");
+});
+module.exports = db;
